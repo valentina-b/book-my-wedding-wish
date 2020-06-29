fffe8a6dcb041e273854d89413fef52c63645af7
valentina-b <valentina.bedi@gmail.com>
2020-06-29 11:51:13 +0000

    Set up the files

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-# 
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'wedding-registry'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+
+@app.route('/')
+def hello():
+    return 'Hello world'
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+
